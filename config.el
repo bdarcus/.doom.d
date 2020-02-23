42cd0b6ff8560c8317912e069661bc730918cae9
Bruce D'Arcus <bdarcus@gmail.com>
2020-02-23 17:33:05 -0500

    reverting to default font

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;(setq doom-font (font-spec :family "monospace" :size 14))
-(setq doom-font (font-spec :family "Fira Mono" :size 14))
+(setq doom-font (font-spec :family "monospace" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
