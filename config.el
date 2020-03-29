a0ad89eb2e471a24a44b15eb0ecddfdef83e8790
Bruce D'Arcus <bdarcus@gmail.com>
2020-03-29 16:08:42 -0400

    changing font to jetbrain-mono

--- config.el
+++ config.el
@@ -20,7 +20,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 14))
+(setq doom-font (font-spec :family "jetbrains-mono" :size 14))
 ;(setq doom-font (font-spec :family "Attribute Mono" :size 14))
 ;(unless (find-font doom-font)
 ;  (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14)))
