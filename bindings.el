a967b68a3e7678e0b894cdb1b86a40ca14deefba
Bruce D'Arcus <bdarcus@gmail.com>
2020-04-03 11:17:43 -0400

    removing separate bindings file

--- bindings.el
+++
@@ -1,8 +0,0 @@
-;;; ~/.doom.d/bindings.el -*- lexical-binding: t; -*-
-
-
-; adding a command to bring up ivy-bibtex for citation insertion
-(map! 
- :map (markdown-mode-map)
- :localleader
- :desc "insert citation" "ic" #'ivy-bibtex)
