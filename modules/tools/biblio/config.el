17e1db4fdb5e74040eabac9d18d08f8825f6f76d
Bruce D'Arcus <bdarcus@gmail.com>
2020-04-10 09:02:49 -0400

    updating text

--- modules/tools/biblio/config.el
+++ modules/tools/biblio/config.el
@@ -4,26 +4,20 @@
   :when (featurep! :completion ivy)
   :commands (ivy-bibtex)
   :config
-  (setq ivy-bibtex-default-action 'ivy-bibtex-insert-key)
+;  (setq ivy-bibtex-default-action 'ivy-bibtex-insert-citation)
   (add-to-list 'ivy-re-builders-alist '(ivy-bibtex . ivy--regex-plus))
   (map! :leader
     :map (org-mode-map markdown-mode-map latex-mode-map)
-    (:prefix("i" . "insert")
-    :desc "Citation" "c" #'ivy-bibtex)))
+    (:prefix("n" . "note")
+    :desc "Bibliograpic entries" "b" #'ivy-bibtex)))
 
 (use-package! helm-bibtex
   :when (featurep! :completion helm)
   :commands helm-bibtex
   :config
-  ; modify so that inserting the citation is the default action
-  ; FIXME doesn't work; not sure why
-  (helm-delete-action-from-source "Insert BibTeX key" helm-source-bibtex)
-  (helm-add-action-to-source "Insert BibTeX key" 'bibtex-completion-insert-key helm-source-bibtex 0)
   (map! :leader
     :map (org-mode-map markdown-mode-map latex-mode-map)
-    (:prefix("i" . "insert")
-    :desc "Citation" "c" #'helm-bibtex)))
+    (:prefix("n" . "note")
+    :desc "Bibliographic entries" "b" #'helm-bibtex)))
 
-; when new org-roam-contrib package is published, turn it on if +roam flag selected on org module
-; (if (featurep! +roam))
-;   (setq org-ref-notes-function #'org-roam-extra-org-ref-notes-fn)
+
