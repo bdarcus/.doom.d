ab67f1b10e8f141042f96aeb9a9e257dc2b8d24b
Bruce D'Arcus <bdarcus@gmail.com>
2020-04-15 15:18:37 -0400

    removing local biblio module

--- modules/tools/biblio/packages.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; tools/biblio/packages.el
-
-(when (featurep! :completion ivy)
-  (package! ivy-bibtex))
-(when (featurep! :completion helm)
-  (package! helm-bibtex))
