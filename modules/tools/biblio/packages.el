0c3d990ae5e9826092dd48aa7c856ca1836ae31a
Bruce D'Arcus <bdarcus@gmail.com>
2021-07-25 19:26:42 -0400

    org-cite

---
+++ modules/tools/biblio/packages.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; -*-
+;;; tools/biblio/packages.el
+
+(package! bibtex-completion :pin "a0d32ab16748b7b0c43d6421f1b497b7caf8e590")
+(when (featurep! :completion ivy)
+  (package! ivy-bibtex :pin "a0d32ab16748b7b0c43d6421f1b497b7caf8e590"))
+(when (featurep! :completion helm)
+  (package! helm-bibtex :pin "a0d32ab16748b7b0c43d6421f1b497b7caf8e590"))
+(when (featurep! :completion vertico)
+  (package! bibtex-actions :pin "6e3a194c3ab655693f8194be78542366755c58c9"))
+
+(package! citeproc :pin "0857973409e3ef2ef0238714f2ef7ff724230d1c")
