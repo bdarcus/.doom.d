8453b05acffaed910172016023fa32b044c8966f
Bruce D'Arcus <bdarcus@gmail.com>
2023-04-05 17:15:55 -0400

    fix: update bindings

--- bindings.el
+++ bindings.el
@@ -2,13 +2,13 @@
 
 (map!
  :leader
- :map bibtex-actions-map ; I don't understand what this line does exactly
+ :map citar-map ; I don't understand what this line does exactly
  :prefix ("B" . "biblio")
- :desc "Insert bibtex"         "b" '("Insert bibtex"       . bibtex-actions-insert-bibtex)
- :desc "Insert citation"       "c" '("Insert citation"     . bibtex-actions-insert-citation)
- :desc "Insert citekey"        "k" '("Insert citekey"      . bibtex-actions-insert-key)
- :desc "Open pdf or link"      "o" '("Open pdf or link"    . bibtex-actions-open)
- :desc "Open link"             "l" '("Open link"           . bibtex-actions-open-link)
- :desc "Open pdf"              "p" '("Open pdf"            . bibtex-actions-open-pdf)
- :desc "Open bibtex entries"   "e" '("Open bibtex entries" . bibtex-actions-open-entry)
- :desc "Open notes"            "n" '("Open notes"          . bibtex-actions-open-notes))
+ :desc "Insert bibtex"         "b" '("Insert bibtex"       . citar-insert-bibtex)
+ :desc "Insert citation"       "c" '("Insert citation"     . citar-insert-citation)
+ :desc "Insert citekey"        "k" '("Insert citekey"      . citar-insert-key)
+ :desc "Open pdf or link"      "o" '("Open pdf or link"    . citar-open)
+ :desc "Open link"             "l" '("Open link"           . citar-open-link)
+ :desc "Open pdf"              "p" '("Open pdf"            . citar-open-pdf)
+ :desc "Open bibtex entries"   "e" '("Open bibtex entries" . citar-open-entry)
+ :desc "Open notes"            "n" '("Open notes"          . citar-open-notes))
